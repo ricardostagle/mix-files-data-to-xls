85e154fd4c8fe46e2e2d57b599e6db90edd09663
Ricardo Sanchez <ricardo.stagle@gmail.com>
2020-02-15 18:00:25 -0600

    commented text for jquery mobile

--- index.php
+++ index.php
@@ -17,9 +17,11 @@ if (!isset($_FILES['file']) && !isset($_FILES['filereport'])) {
   <meta charset="UTF-8">
   <meta http-equiv="Content-Type" content="text/html;" charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- // comment some lines to unable styles
   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
   <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+  -->
 </head>
   <body style="position: relative; margin:0 auto; width:80%">
     <h1>Generador de nuevo reporte.</h1>
